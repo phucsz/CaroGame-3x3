fe8de2167d99c6d635f9b02623de57d70047e711
phucsz <pphphuc1509@gmail.com>
2026-09-12 14:08:38 +0700

    button and point

--- src/caro/start.java
+++ src/caro/start.java
@@ -1,13 +1,18 @@
 package caro;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -17,16 +22,20 @@ public class start extends JFrame implements ActionListener{
 	 */
 	private static final long serialVersionUID = 1L;
 	private JButton[][] btn;
-	private JButton btnNewGame;
+	private JButton btnNewGame,btnResetPoint,btnExitGame;
 	private int chk = 0;
+	private int Opts,Xpts;
 	private int[][] a;
+	private JLabel lblXpts = new JLabel("X points: " + Xpts);
+	private JLabel lblOpts = new JLabel("O points: " + Opts);
+	
 	public start() {
-		setSize(500,500);
-		setTitle("Caro");
+		setSize(500,600);
+		setTitle("Caro Game 3x3");
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
-		
+		setLayout(new BorderLayout());
 		JPanel p = new JPanel();
 		p.setLayout(new GridLayout(4, 3));	
 		btn = new JButton[3][3];
@@ -43,10 +52,40 @@ public class start extends JFrame implements ActionListener{
 				});
 			}
 		}
+		
+		//Panel chứa nút và thống kê
+		JPanel pBoard = new JPanel();
+		pBoard.setLayout(new BoxLayout(pBoard, BoxLayout.X_AXIS));
+		add(pBoard,BorderLayout.SOUTH);
+		
+		//Các button chức năng
 		JPanel pButton = new JPanel();
-		p.add(pButton);
+		pButton.setLayout(new FlowLayout());
 		pButton.add(btnNewGame = new JButton("New Game"));
 		btnNewGame.addActionListener(e -> ResetGame());
+		pButton.add(btnResetPoint = new JButton("Reset Point"));
+		btnResetPoint.addActionListener(e -> ResetPoint());
+		pButton.add(btnExitGame = new JButton("Exit"));
+		btnExitGame.addActionListener(e -> { System.exit(0);});
+		
+		//Thống kê ingame
+		JPanel pStats = new JPanel();
+		pStats.setLayout(new BoxLayout(pStats, BoxLayout.Y_AXIS));
+		
+		lblXpts.setAlignmentX(Component.CENTER_ALIGNMENT);
+		lblOpts.setAlignmentX(Component.CENTER_ALIGNMENT);
+		pStats.add(Box.createVerticalGlue());
+		pStats.add(lblXpts);
+		pStats.add(Box.createVerticalStrut(10)); 
+		pStats.add(lblOpts);
+		pStats.add(Box.createVerticalStrut(50)); 
+		lblOpts.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+		lblXpts.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+		
+		pBoard.add(pButton);
+		pBoard.add(pStats);
+		
+		
 	}
 	
 	public static void main(String[] args) {
@@ -62,6 +101,8 @@ public class start extends JFrame implements ActionListener{
 			btn.setForeground(Color.RED);
 			if (CheckWin(row,col,chk)) {
 				JOptionPane.showMessageDialog(this,"O WIN");
+				ResetGame();
+				UpdatePoints(lblOpts,'O');
 				return;
 			}
 		}
@@ -72,11 +113,14 @@ public class start extends JFrame implements ActionListener{
 			btn.setForeground(Color.black);
 			if (CheckWin(row,col,chk)) {
 				JOptionPane.showMessageDialog(this,"X WIN");
+				ResetGame();
+				UpdatePoints(lblXpts,'X');
 				return;
 			}
 		}
 		if (CheckDraw()) {
 			JOptionPane.showMessageDialog(this,"DRAW");
+			ResetGame();
 			return;
 		}
 	}
@@ -108,7 +152,24 @@ public class start extends JFrame implements ActionListener{
 				btn[i][j].setText("");
 			}
 		}
-		
+	}
+	public void UpdatePoints(JLabel pts,char check) {
+		int plusPoint;
+		if (check == 'X') {
+			Xpts++;
+			plusPoint = Xpts;
+		}
+		else {
+			Opts++;
+			plusPoint = Opts;
+		}
+		pts.setText(check+ " points: " + (plusPoint));
+	}
+	public void ResetPoint() {
+		Xpts = 0;
+		Opts = 0;
+		lblXpts.setText("X points: " + Xpts);
+		lblOpts.setText("O points: " + Opts);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
